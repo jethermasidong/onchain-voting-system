7fa0469e7abfd5d6678639cd2811e0fb4a5f9012
jethermasidong <jethermasidong05@gmail.com>
2026-03-10 11:25:22 +0800

    Progress: Fixed Minor Code

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -4,9 +4,9 @@ import Candidates from "../models/candidateModel.js";
 export const insert = async (req, res) => {
     try {
         const {full_name, position, partylist} = req.body;
-        const newCandidate = await Candidates.insert(full_name, position, partylist);
+        const result = await Candidates.insert(full_name, position, partylist);
         
-        return res.status(201).json({message: 'Candidate create successfully', candidate: newCandidate});
+        return res.status(201).json({message: 'Candidate create successfully', candidate: result.insertId});
     
     } catch (err) {
         console.error("Insert Candidate Error:", err);
